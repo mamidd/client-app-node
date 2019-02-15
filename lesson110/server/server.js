7d1de50be89ca16baaef7129f973b14c0fa0810a
Mario De Donno <mamidd@gmail.com>
2019-02-15 19:29:49 +0100

    fix: try to fix heroku deploy

--- lesson110/server/server.js
+++ lesson110/server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-const socketIO = require('socket.io');
+const socketIO = require('Socket.io');
 
 const publicPath = path.join(__dirname,'..','public');
 const port = process.env.PORT || 3000;
