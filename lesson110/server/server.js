fe7c9d71045f72f0d4f2f8dc30c504f5e76578fb
Mario De Donno <mamidd@gmail.com>
2019-02-15 19:32:35 +0100

    fix: missing socket.io dependency

--- lesson110/server/server.js
+++ lesson110/server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-const socketIO = require('Socket.io');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'..','public');
 const port = process.env.PORT || 3000;
