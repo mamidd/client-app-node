8035f04db84c71e4f24151ffa52c6ec70c8f319c
Mario De Donno <mamidd@gmail.com>
2019-02-20 21:47:03 +0100

    feat: add form, form messages and show incoming messages into browser

---
+++ lesson113/server/server.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const {generateMessage} = require('./utils/message');
+const publicPath = path.join(__dirname,'..','public');
+const port = process.env.PORT || 3000;
+
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+app.use(express.static(publicPath))
+
+io.on('connection', (socket) => {
+  console.log(`New user connected`);
+
+  socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app!'))
+
+  socket.broadcast.emit('newMessage',generateMessage('Admin','New User joined to chat app!'))
+
+  socket.on('createMessage', (message, callback) => {
+    console.log('Create Message', message);
+    io.emit('newMessage', generateMessage(message.from,message.text));
+    callback('This is from the server.');
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createAt: new Date().getTime()
+    // })
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`User was disconnected`);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
+module.exports.app = app;
