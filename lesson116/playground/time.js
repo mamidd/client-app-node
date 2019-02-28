e47b6e9b8a1a53d523142e4adb5f10ce2a20ab5a
Mario De Donno <mamidd@gmail.com>
2019-02-28 22:59:45 +0100

    feat: add responsive css

---
+++ lesson116/playground/time.js
@@ -0,0 +1,11 @@
+const moment = require('moment');
+
+// let date = new Date();
+// console.log(date.getMonth());
+
+// let date = moment();
+// date.add(1, 'years');
+// console.log(date.format('MMM YYYY'));
+
+let date = moment();
+console.log(date.format('h:mm a'));
