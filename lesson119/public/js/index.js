5fe89941cb0b99f2473b86bbde9e1a250b39ce84
Mario De Donno <mamidd@gmail.com>
2019-03-12 19:19:14 +0100

    feat: add mustache messages template

---
+++ lesson119/public/js/index.js
@@ -0,0 +1,78 @@
+let socket = io();
+
+socket.on('connect', function () {
+  console.log('Connected to server');
+});
+
+socket.on('disconnect', function (){
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message){
+  console.log('Got New Message. ', message);
+
+  renderMessage('#message-template', {
+    text: message.text,
+    from: message.from,
+    createdAt: formatTime(message.createdAt)
+  });
+});
+
+socket.on('newLocationMessage', function (message){
+  console.log('Got New Location Message. ', message);
+
+  renderMessage('#location-message-template', {
+    url: message.url,
+    from: message.from,
+    createdAt: formatTime(message.createdAt)
+  });
+});
+
+let messageTextBox = jQuery('[name=message]');
+
+jQuery('#message-form').on('submit', function (e){
+  let _this = this;
+  e.preventDefault();
+  socket.emit('createMessage', {
+    from: 'User',
+    text: messageTextBox.val()
+  }, function (data) {
+    console.log('Got it. ', data);
+    messageTextBox.val('')
+  })
+});
+
+let locationButton = jQuery('#send-location');
+locationButton.on('click', function(e) {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+
+  locationButton.attr('disabled', true).text('Sending location...');
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    }, function (data) {
+      console.log(data);
+      locationButton.attr('disabled', false).text('Send location');
+    })
+
+  }, function () {
+    alert('Unable to fetch location.');
+    locationButton.attr('disabled', false).text('Send location');
+  });
+});
+
+function formatTime(time) {
+  return moment(time).format('h:mm a');
+}
+
+function renderMessage(templateName, templateParameters){
+  let template = jQuery(templateName).html();
+  let html = Mustache.render(template, templateParameters);
+
+  jQuery('#messages').append(html);
+}
