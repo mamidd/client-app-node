644ec74363af1515fc4a2056a13c1d69c49fab07
Mario De Donno <mamidd@gmail.com>
2019-03-15 20:16:50 +0100

    feat: add first room management

---
+++ lesson122/server/server.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
+
+const publicPath = path.join(__dirname,'..','public');
+const port = process.env.PORT || 3000;
+
+let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+app.use(express.static(publicPath))
+
+io.on('connection', (socket) => {
+  console.log(`New user connected`);
+
+  socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app!'))
+
+  socket.broadcast.emit('newMessage',generateMessage('Admin','New User joined to chat app!'))
+
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)){
+      return callback('Name and room name are required.');
+    }
+
+    callback();
+  });
+
+  socket.on('createMessage', (message, callback) => {
+    console.log('Create Message', message);
+    io.emit('newMessage', generateMessage(message.from,message.text));
+    callback('Message sent to all users.');
+  });
+
+  socket.on('createLocationMessage', (coords, callback) => {
+    console.log('Create Location Message', coords);
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    callback('Location sent to all users.');
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`User was disconnected`);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
+module.exports.app = app;
